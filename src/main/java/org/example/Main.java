96dbfc2218cdd54417c6b24a74d21fa6c6d3d5ad
evoss5 <116517344+evoss5@users.noreply.github.com>
2024-01-31 11:28:32 +0100

    Ćwiczenie 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        for (int i = 0; i <= 0; i++) {
+            System.out.println("Pierwszy element z listy to: " + list.get(0));
+            System.out.println("Drugi element z listy to : " + list.get(1));
+            System.out.println("Trzeci element z listy to: " + list.get(2));
+        }
     }
 }
